5604990cbe61f236c162a00184961fe57ac5bca9
User61 <svetsoff@gmail.com>
2023-01-16 15:06:08 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,8 +6,7 @@ const getRandomHexColor = () =>
 
 export const Statistics = ({ title, stats }) => (
   <section className={css.statistics}>
-    {title ? <h2 className={css.title}>{title}</h2> : ''}
-
+    {title && <h2 className={css.title}>{title}</h2>}
     <ul className={css.statlist}>
       {stats.map(({ id, label, percentage }) => (
         <li
